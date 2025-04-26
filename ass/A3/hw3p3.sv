03fa145399a8a9c986adc3e977595554eec8015f
dankank22 <ankithsworld@gmail.com>
2025-04-25 17:36:31 -0700

    Update hw3p3 and testbench

--- ass/A3/hw3p3.sv
+++ ass/A3/hw3p3.sv
@@ -1,6 +1,45 @@
 /* Arbitrary ASM chart implementation to examine output timings */
-module hw3p3 (clk, reset, X, Ya, Yb, Yc, Z1, Z2);
+module hw3p3 (
+    input  logic clk,
+    input  logic reset,
+    input  logic X,
+    output logic Ya, Yb, Yc,
+    output logic Z1, Z2);
 	
-	// for you to implement
+	 typedef enum logic [1:0] { S0, S1, S2 } state_t; //three states
+
+    state_t ps, ns;  //ps and ns according to assignment spec
+	 
+	  always_ff @(posedge clk) begin //reset 
+        if (reset)  ps <= S0;
+        else        ps <= ns;
+     end
+	  
+	  always_comb begin
+	      ns = ps;
+			Z1 = 1'b0;
+			Z2 = 1'b0;
+			
+			case(ps) 
+			   S0: begin
+				    ns = X ? S1 : S0;
+					 end
+				S1: begin
+				    ns = X ? S2 : S0;
+					 end		
+				S2 : begin
+                 if (X) begin
+                     Z2 = 1'b1;   
+                 end
+                 else begin
+                     Z1 = 1'b1;
+                 end
+                 ns = S0;         
+            end
+		  endcase
+	  end
+	  assign Ya = (ps == S0);
+	  assign Yb = (ps == S1);
+	  assign Yc = (ps == S2);
 	
 endmodule  // hw3p3
--- ass/A3/hw3p3_tb.sv
+++ ass/A3/hw3p3_tb.sv
@@ -1,14 +1,51 @@
 /* Testbench for Homework 3 Problem 3 */
-module hw3p3_tb ();
-	
-	// for you to implement
-	
-	hw3p3 dut (.*);
-	
-	initial begin
-		
-		// for you to implement
-		
-	end  // initial
-	
-endmodule  // hw3p3_tb
+module hw3p3_tb;
+
+   logic clk, reset;
+   logic X;
+   logic Ya, Yb, Yc, Z1, Z2;
+
+   hw3p3 dut (.*);
+
+   parameter PERIOD = 10;
+   integer i;
+   initial begin
+      clk = 0;
+      for (i = 0; i < 200; i++) begin
+         #(PERIOD/2) clk <= ~clk;
+      end
+   end
+
+   initial begin
+      X <= 0;
+      reset <= 1;  //reset 
+      @(posedge clk);
+      reset <= 0;
+      @(posedge clk);
+
+      X <= 1; @(posedge clk); //s0 -> s1
+      X <= 1; @(posedge clk); //s1 -> s2
+      assert (Z2 == 1 && Z1 == 0 && Ya == 0 && Yb == 0 && Yc == 1); //s2 -> s2
+      @(posedge clk);
+
+      X <= 1; @(posedge clk); //s0 -> s1
+      X <= 1; @(posedge clk); //s1 -> s2
+      X <= 0; #1; //Z1 =  1 but X = 0
+      assert (Z1 == 1 && Z2 == 0 && Yc == 1);  // still in S2
+      @(posedge clk);
+
+      X <= 1; @(posedge clk); //s0 -> s1
+      X <= 0; @(posedge clk); //s1 -> s0
+      assert (Z1 == 0 && Z2 == 0 && Ya == 1); // no Z outputs
+
+      repeat (2) begin
+         X <= 1; @(posedge clk); //s0 -> s1
+         X <= 1; @(posedge clk); //s1 -> s2
+         assert (Z2 == 1); //make sure Z2 is on through 2 cycles of consecutive X=1
+         @(posedge clk);
+      end
+
+      $stop;
+   end
+
+endmodule // hw3p3_tb
